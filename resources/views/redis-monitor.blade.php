a9d036555721d903cf4a81c9ff72eee0f9413b0f
Raphael van den Doel <doelraphael@gmail.com>
2024-08-06 16:58:55 +0200

    Show memory usage in custom pulse card

--- resources/views/redis-monitor.blade.php
+++ resources/views/redis-monitor.blade.php
@@ -1,7 +1,6 @@
 <x-pulse::card id="redis-monitor-card" :cols="$cols" :rows="$rows" :class="$class">
     <x-pulse::card-header
         name="Redis"
-        title="Time: {{ number_format($time) }}ms; Run at: {{ $runAt }};"
         details="past {{ $this->periodForHumans() }}"
     >
         <x-slot:icon>
@@ -10,10 +9,123 @@
     </x-pulse::card-header>
 
     <x-pulse::scroll :expand="$expand" wire:poll.5s="">
-        @if ($errors->isEmpty())
+        @if ($memory->isEmpty())
             <x-pulse::no-results />
         @else
-            <!-- Add card here -->
+        <div class="grid gap-3 mx-px mb-px">
+            @foreach ($memory as $connection => $data)
+                <div wire:key="connections{{ $connection }}">
+                    <h3 class="font-bold text-gray-700 dark:text-gray-300">
+                        {{ $connection }}
+                    </h3>
+                    <div class="mt-3 relative">
+                        <div
+                            wire:ignore
+                            class="h-14"
+                            x-data="redisMonitorChart({
+                                connection: '{{ $connection }}',
+                                data: @js($data)
+                            })"
+                        >
+
+                        <canvas x-ref="canvas" class="ring-1 ring-gray-900/5 dark:ring-gray-100/10 bg-gray-50 dark:bg-gray-800 rounded-md shadow-sm"></canvas>
+                    </div>
+                </div>
+            @endforeach
+        </div>
         @endif
     </x-pulse::scroll>
 </x-pulse::card>
+
+@script
+<script>
+Alpine.data('redisMonitorChart', (config) => ({
+    init() {
+        let chart = new Chart(
+            this.$refs.canvas,
+            {
+                type: 'line',
+                data: {
+                    labels: this.labels(config.data),
+                    datasets: [
+                        {
+                            label: 'Used memory',
+                            borderColor: '#ff0000',
+                            data: config.data['used_memory'],
+                            order: 0,
+                        },
+                        {
+                            label: 'Max memory',
+                            borderColor: '#000000',
+                            data: config.data['max_memory'],
+                            order: 1,
+                        },
+                    ],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    layout: {
+                        autoPadding: false,
+                        padding: {
+                            top: 1,
+                        },
+                    },
+                    datasets: {
+                        line: {
+                            borderWidth: 2,
+                            borderCapStyle: 'round',
+                            pointHitRadius: 10,
+                            pointStyle: false,
+                            tension: 0.2,
+                            spanGaps: false,
+                            segment: {
+                                borderColor: (ctx) => ctx.p0.raw === 0 && ctx.p1.raw === 0 ? 'transparent' : undefined,
+                            }
+                        }
+                    },
+                    scales: {
+                        x: {
+                            display: false,
+                        },
+                        y: {
+                            display: false,
+                            min: 0,
+                            max: this.highest(config.data),
+                        },
+                    },
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                    },
+                },
+            }
+        )
+
+        Livewire.on('redis-monitor-chart-update', ({ memory }) => {
+            if (chart === undefined) {
+                return
+            }
+
+            if (memory[config.connection] === undefined && chart) {
+                chart.destroy()
+                chart = undefined
+                return
+            }
+
+            chart.data.labels = this.labels(memory[config.connection])
+            chart.options.scales.y.max = this.highest(memory[config.connection])
+            chart.data.datasets[0].data = memory[config.connection]['used_memory']
+            chart.data.datasets[1].data = memory[config.connection]['max_memory']
+            chart.update()
+        })
+    },
+    labels(data) {
+        return Object.keys(data['used_memory'])
+    },
+    highest(readings) {
+        return Math.max(...Object.values(readings).map(dataset => Math.max(...Object.values(dataset))))
+    }
+}))
+</script>
+@endscript
--- src/Cards/RedisMonitor.php
+++ src/Cards/RedisMonitor.php
@@ -3,18 +3,25 @@
 namespace PraatmetdeDokter\Pulse\RedisMonitor\Cards;
 
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\View;
+use Laravel\Pulse\Facades\Pulse;
 use Laravel\Pulse\Livewire\Card;
 use Livewire\Attributes\Lazy;
 
-/**
- * @internal
- */
 #[Lazy]
 class RedisMonitor extends Card
 {
     public function render(): Renderable
     {
-        return View::make('redis-monitor::redis-monitor');
+        $memory = Pulse::graph(['used_memory', 'max_memory'], 'avg', $this->periodAsInterval());
+
+        if (Request::hasHeader('X-Livewire')) {
+            $this->dispatch('redis-monitor-chart-update', memory: $memory);
+        }
+
+        return View::make('redis-monitor::redis-monitor', [
+            'memory' => $memory
+        ]);
     }
 }
