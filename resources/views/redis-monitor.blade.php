18879555c9faf7c055c084441176a7592c3a48f4
Raphael van den Doel <doelraphael@gmail.com>
2024-08-07 17:00:37 +0200

    Add removed key and network data graphs

--- resources/views/redis-monitor.blade.php
+++ resources/views/redis-monitor.blade.php
@@ -37,8 +37,8 @@
         </div>
 
         <div class="grid gap-3 mx-px mb-px">
-            <h3 class="font-bold text-gray-700 dark:text-gray-300">Keys</h3>
-            @foreach ($keys as $connection => $data)
+            <h3 class="font-bold text-gray-700 dark:text-gray-300">Active keys</h3>
+            @foreach ($active_keys as $connection => $data)
                 <div wire:key="keys-connection-{{ $connection }}">
                     <h3 class="font-bold text-gray-700 dark:text-gray-300">
                         {{ $connection }}
@@ -47,7 +47,31 @@
                         <div
                             wire:ignore
                             class="h-14"
-                            x-data="redisMonitorKeysChart({
+                            x-data="redisMonitorActiveKeysChart({
+                                connection: '{{ $connection }}',
+                                colors: @js($colors),
+                                data: @js($data)
+                            })"
+                        >
+
+                        <canvas x-ref="canvas" class="ring-1 ring-gray-900/5 dark:ring-gray-100/10 bg-gray-50 dark:bg-gray-800 rounded-md shadow-sm"></canvas>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="grid gap-3 mx-px mb-px">
+            <h3 class="font-bold text-gray-700 dark:text-gray-300">Removed keys</h3>
+            @foreach ($removed_keys as $connection => $data)
+                <div wire:key="keys-connection-{{ $connection }}">
+                    <h3 class="font-bold text-gray-700 dark:text-gray-300">
+                        {{ $connection }}
+                    </h3>
+                    <div class="mt-3 relative">
+                        <div
+                            wire:ignore
+                            class="h-14"
+                            x-data="redisMonitorRemovedKeysChart({
                                 connection: '{{ $connection }}',
                                 colors: @js($colors),
                                 data: @js($data)
@@ -72,6 +96,30 @@
                             wire:ignore
                             class="h-14"
                             x-data="redisMonitorTtlChart({
+                                connection: '{{ $connection }}',
+                                colors: @js($colors),
+                                data: @js($data)
+                            })"
+                        >
+
+                        <canvas x-ref="canvas" class="ring-1 ring-gray-900/5 dark:ring-gray-100/10 bg-gray-50 dark:bg-gray-800 rounded-md shadow-sm"></canvas>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="grid gap-3 mx-px mb-px">
+            <h3 class="font-bold text-gray-700 dark:text-gray-300">Network usage</h3>
+            @foreach ($network as $connection => $data)
+                <div wire:key="keys-connection-{{ $connection }}">
+                    <h3 class="font-bold text-gray-700 dark:text-gray-300">
+                        {{ $connection }}
+                    </h3>
+                    <div class="mt-3 relative">
+                        <div
+                            wire:ignore
+                            class="h-14"
+                            x-data="redisMonitorNetworkUsageChart({
                                 connection: '{{ $connection }}',
                                 colors: @js($colors),
                                 data: @js($data)
@@ -202,7 +250,7 @@ Alpine.data('redisMonitorMemoryChart', (config) => ({
     }
 }))
 
-Alpine.data('redisMonitorKeysChart', (config) => ({
+Alpine.data('redisMonitorActiveKeysChart', (config) => ({
     init() {
         let chart = new Chart(
             this.$refs.canvas,
@@ -221,6 +269,106 @@ Alpine.data('redisMonitorKeysChart', (config) => ({
                             label: 'Total keys',
                             borderColor: config.colors['primary'],
                             data: config.data['keys_total'],
+                            order: 1,
+                        }
+                    ],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    layout: {
+                        autoPadding: false,
+                        padding: {
+                            top: 1,
+                        },
+                    },
+                    datasets: {
+                        line: {
+                            borderWidth: 2,
+                            borderCapStyle: 'round',
+                            pointHitRadius: 10,
+                            pointStyle: false,
+                            tension: 0.2,
+                            spanGaps: false,
+                            segment: {
+                                borderColor: (ctx) => ctx.p0.raw === 0 && ctx.p1.raw === 0 ? 'transparent' : undefined,
+                            }
+                        }
+                    },
+                    scales: {
+                        x: {
+                            display: false,
+                        },
+                        y: {
+                            display: false,
+                            min: 0,
+                            max: this.highest(config.data),
+                        },
+                    },
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                        tooltip: {
+                            mode: 'index',
+                            position: 'nearest',
+                            intersect: false,
+                            callbacks: {
+                                beforeBody: (context) => context
+                                    .map(item => `${item.dataset.label}: ${item.formattedValue}`)
+                                    .join(', '),
+                                label: () => null,
+                            },
+                        },
+                    },
+                },
+            }
+        )
+
+        Livewire.on('redis-monitor-active-keys-chart-update', ({ keys }) => {
+            if (chart === undefined) {
+                return
+            }
+
+            if (keys[config.connection] === undefined && chart) {
+                chart.destroy()
+                chart = undefined
+                return
+            }
+
+            chart.data.labels = this.labels(keys[config.connection])
+            chart.options.scales.y.max = this.highest(keys[config.connection])
+            chart.data.datasets[0].data = keys[config.connection]['keys_with_expiration']
+            chart.data.datasets[1].data = keys[config.connection]['keys_total']
+            chart.update()
+        })
+    },
+    labels(data) {
+        return Object.keys(data['keys_total'])
+    },
+    highest(readings) {
+        return Math.max(...Object.values(readings).map(dataset => Math.max(...Object.values(dataset))))
+    }
+}))
+
+Alpine.data('redisMonitorRemovedKeysChart', (config) => ({
+    init() {
+        let chart = new Chart(
+            this.$refs.canvas,
+            {
+                type: 'line',
+                data: {
+                    labels: this.labels(config.data),
+                    datasets: [
+                        {
+                            label: 'Expired keys',
+                            borderColor: config.colors['secondary'],
+                            data: config.data['expired_keys'],
+                            order: 0,
+                        },
+                        {
+                            label: 'Evicted keys',
+                            borderColor: config.colors['primary'],
+                            data: config.data['evicted_keys'],
                             order: 1,
                         },
                     ],
@@ -276,7 +424,7 @@ Alpine.data('redisMonitorKeysChart', (config) => ({
             }
         )
 
-        Livewire.on('redis-monitor-keys-chart-update', ({ keys }) => {
+        Livewire.on('redis-monitor-removed-keys-chart-update', ({ keys }) => {
             if (chart === undefined) {
                 return
             }
@@ -289,13 +437,13 @@ Alpine.data('redisMonitorKeysChart', (config) => ({
 
             chart.data.labels = this.labels(keys[config.connection])
             chart.options.scales.y.max = this.highest(keys[config.connection])
-            chart.data.datasets[0].data = keys[config.connection]['keys_with_expiration']
-            chart.data.datasets[1].data = keys[config.connection]['keys_total']
+            chart.data.datasets[0].data = keys[config.connection]['expired_keys']
+            chart.data.datasets[1].data = keys[config.connection]['evicted_keys']
             chart.update()
         })
     },
     labels(data) {
-        return Object.keys(data['keys_total'])
+        return Object.keys(data['expired_keys'])
     },
     highest(readings) {
         return Math.max(...Object.values(readings).map(dataset => Math.max(...Object.values(dataset))))
@@ -427,6 +575,112 @@ Alpine.data('redisMonitorTtlChart', (config) => ({
         }
 
         return label.trim()
+    }
+}))
+
+Alpine.data('redisMonitorNetworkUsageChart', (config) => ({
+    init() {
+        let chart = new Chart(
+            this.$refs.canvas,
+            {
+                type: 'line',
+                data: {
+                    labels: this.labels(config.data),
+                    datasets: [
+                        {
+                            label: 'Network usage',
+                            borderColor: config.colors['secondary'],
+                            data: config.data['redis_network_usage'],
+                            order: 0,
+                        }
+                    ],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    layout: {
+                        autoPadding: false,
+                        padding: {
+                            top: 1,
+                        },
+                    },
+                    datasets: {
+                        line: {
+                            borderWidth: 2,
+                            borderCapStyle: 'round',
+                            pointHitRadius: 10,
+                            pointStyle: false,
+                            tension: 0.2,
+                            spanGaps: false,
+                            segment: {
+                                borderColor: (ctx) => ctx.p0.raw === 0 && ctx.p1.raw === 0 ? 'transparent' : undefined,
+                            }
+                        }
+                    },
+                    scales: {
+                        x: {
+                            display: false,
+                        },
+                        y: {
+                            display: false,
+                            min: 0,
+                            max: this.highest(config.data),
+                        },
+                    },
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                        tooltip: {
+                            mode: 'index',
+                            position: 'nearest',
+                            intersect: false,
+                            callbacks: {
+                                beforeBody: (context) => context
+                                    .map(item => `${item.dataset.label}: ${this.labelValue(item.formattedValue)}`)
+                                    .join(', '),
+                                label: () => null,
+                            },
+                        },
+                    },
+                },
+            }
+        )
+
+        Livewire.on('redis-monitor-network-usage-chart-update', ({ network }) => {
+            if (chart === undefined) {
+                return
+            }
+
+            if (network[config.connection] === undefined && chart) {
+                chart.destroy()
+                chart = undefined
+                return
+            }
+
+            chart.data.labels = this.labels(network[config.connection])
+            chart.options.scales.y.max = this.highest(network[config.connection])
+            chart.data.datasets[0].data = network[config.connection]['redis_network_usage']
+            chart.update()
+        })
+    },
+    labels(data) {
+        return Object.keys(data['redis_network_usage'])
+    },
+    highest(readings) {
+        return Math.max(...Object.values(readings).map(dataset => Math.max(...Object.values(dataset))))
+    },
+    labelValue(bytesString) {
+        bytes = parseFloat(bytesString.replace(/\./g, '').replace(',', '.'))
+        if (bytes === 0) {
+            return '0 Bytes'
+        }
+
+        const k = 1024
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        const i = Math.floor(Math.log(bytes) / Math.log(k))
+        const value = parseFloat((bytes / Math.pow(k, i)).toFixed(2))
+
+        return `${value} ${sizes[i]}`
     }
 }))
 </script>
--- src/Cards/RedisMonitor.php
+++ src/Cards/RedisMonitor.php
@@ -33,19 +33,25 @@ class RedisMonitor extends Card
     public function render(): Renderable
     {
         $memory = Pulse::graph(['used_memory', 'max_memory'], 'avg', $this->periodAsInterval());
-        $keys = Pulse::graph(['keys_total', 'keys_with_expiration'], 'avg', $this->periodAsInterval());
+        $active_keys = Pulse::graph(['keys_total', 'keys_with_expiration'], 'avg', $this->periodAsInterval());
+        $removed_keys = Pulse::graph(['expired_keys', 'evicted_keys'], 'avg', $this->periodAsInterval());
         $avg_ttl = Pulse::graph(['avg_ttl'], 'avg', $this->periodAsInterval());
+        $network = Pulse::graph(['redis_network_usage'], 'avg', $this->periodAsInterval());
 
         if (Request::hasHeader('X-Livewire')) {
             $this->dispatch('redis-monitor-memory-chart-update', memory: $memory);
-            $this->dispatch('redis-monitor-keys-chart-update', keys: $keys);
+            $this->dispatch('redis-monitor-active-keys-chart-update', keys: $active_keys);
+            $this->dispatch('redis-monitor-removed-keys-chart-update', keys: $removed_keys);
             $this->dispatch('redis-monitor-ttl-chart-update', avg_ttl: $avg_ttl);
+            $this->dispatch('redis-monitor-network-usage-chart-update', network: $network);
         }
 
         return View::make('redis-monitor::redis-monitor', [
             'memory' => $memory,
-            'keys' => $keys,
+            'active_keys' => $active_keys,
+            'removed_keys' => $removed_keys,
             'avg_ttl' => $avg_ttl,
+            'network' => $network,
             'colors' => $this->colors
         ]);
     }
